9c051bd4dcc0ffe3db3fa1b7f83f34d06f07d5cc
quangg1007 <quangphammrr19@gmail.com>
2025-02-07 13:24:40 +0700

    Update API URL

--- src/environments/enviroment.ts
+++ src/environments/enviroment.ts
@@ -1,5 +1,5 @@
-// const APIUrl = 'https://flightbookingbe-production-967d.up.railway.app/api';
-const APIUrl = 'http://localhost:8081/api';
+const APIUrl = 'https://flightbookingbe-production-967d.up.railway.app/api';
+// const APIUrl = 'http://localhost:8081/api';
 const TIMEZONE_KEY = 'userTimezone';
 
 export { APIUrl, TIMEZONE_KEY };
